f05ddc8abee5a87708782119980968aa71d395bc
5yyyaaazzz-source <5yyyaaazzz@gmail.com>
2026-09-06 11:28:12 +0530

    Add homepage

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from '@/components/home/Hero';
+import NewDrop from '@/components/home/NewDrop';
+import CustomSection from '@/components/home/CustomSection';
+import TrustSection from '@/components/home/TrustSection';
+import Instagram from '@/components/home/Instagram';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <NewDrop />
+      <CustomSection />
+      <TrustSection />
+      <Instagram />
+    </>
+  );
+}
